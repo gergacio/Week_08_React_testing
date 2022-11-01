fbb005fcd677523659fac093fca0f92c71a86852
Georgi <ggeorgeuk@gmail.com>
2022-11-01 09:51:15 +0000

    commit

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -14,11 +14,34 @@ describe("Calculator", () => {
     cy.get("#operator-equals").click();
     cy.get(".display").should("contain", "5");
   });
-  it("should have arithmetical operations update the display with the result of the operation", () => {
-    cy.get("#number4").click();
+  xit("should have arithmetical operations update the display with the result of the operation", () => {
+    cy.get("#number8").click();
     cy.get("#operator-divide").click();
     cy.get("#number2").click();
     cy.get("#operator-equals").click();
-    cy.get(".display").should("contain", "2");
+    cy.get(".display").should("contain", "4");
   });
+  xit("should have multiple operations be chained together", () => {
+    cy.get("#number1").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number9").click();
+    cy.get("#operator-divide").click();
+    cy.get("#number3").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "3");
+  });
+  xit("should be able to display negative numbers", () => {
+    cy.get("#number3").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number6").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "-3");
+  });
+  xit("should be able to display decimal numbers", () => {
+    cy.get("#number5").click();
+    cy.get("#decimal").click();
+    cy.get("#number3").click();
+    cy.get(".display").should("contain", "5.3");
+  });
+
 })
